492564c925e2f7f5386765d94c0d79c33ffa4c53
myrthe.e <myrthe.e@hotmail.nl>
2019-10-26 13:17:58 +0200

    Title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    DELICIOUS COFFEE
                 </div>
 
                 <div class="links">
