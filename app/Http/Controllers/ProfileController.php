5e92e3ccb96559ee67541613ca01d83c2152a02a
myrthe.e <myrthe.e@hotmail.nl>
2019-10-29 12:44:13 +0100

    Added profiles

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,22 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/spots', 'SpotController@index')->name('spots');
-Route::get('/spots/create', 'SpotController@create')->name('spot.create');
-Route::post('/spots', 'SpotController@store')->name('spot.store');
-Route::get('/spots/{spot}', 'SpotController@show')->name('spot.show');
-Route::get('/spots/{spot}/edit', 'SpotController@edit')->name('spot.edit');
-Route::post('/spots/{spot}/update', 'SpotController@update')->name('spot.update');
-Route::delete('/spots/{spot}', 'SpotController@destroy')->name('spot.destroy');
+Route::prefix('/spots')->group(function(){
+    Route::get('/', 'SpotController@index')->name('spots');
+    Route::get('/create', 'SpotController@create')->name('spot.create');
+    Route::post('/', 'SpotController@store')->name('spot.store');
+    Route::get('/{spot}', 'SpotController@show')->name('spot.show');
+    Route::get('/{spot}/edit', 'SpotController@edit')->name('spot.edit');
+    Route::post('/{spot}/update', 'SpotController@update')->name('spot.update');
+    Route::delete('/{spot}', 'SpotController@destroy')->name('spot.destroy');
+});
 
 Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('can:manage-users')->group(function(){
     Route::resource('/users', 'UsersController', ['except' => ['show', 'create', 'store']]);
+});
+
+Route::prefix('/profile')->group(function(){
+    Route::get('/', 'ProfileController@index')->name('profile.show');
+    Route::get('/edit', 'ProfileController@edit')->name('profile.edit');
+    Route::get('/', 'ProfileController@update')->name('profile.update');
 });
